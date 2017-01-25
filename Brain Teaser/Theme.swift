286186c1a41e017c0ec1e93b0d0b793047463b41
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-25 23:58:36 +0300

    Add theme file to handle app theming

---
+++ Brain Teaser/Theme.swift
@@ -0,0 +1,19 @@
+//
+//  Theme.swift
+//  Brain Teaser
+//
+//  Created by Muhammad Moaz on 1/25/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+
+struct Theme {
+    static var correctAnswerColor: UIColor {
+        return UIColor(red: 0.27, green: 0.69, blue: 0.41, alpha: 1.00)
+    }
+    
+    static var wrongAnswerColor: UIColor {
+        return UIColor(red: 0.97, green: 0.20, blue: 0.24, alpha: 1.00)
+    }
+}
