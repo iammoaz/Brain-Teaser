a85981ab1a08cb8a4b9353958944b571e0ee871f
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-25 21:35:32 +0300

    Change ViewController to LandingController

--- Brain Teaser/LandingController.swift
+++ Brain Teaser/LandingController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LandingController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startButtonTapped(sender: UIButton) {
+        performSegue(withIdentifier: "GameController", sender: nil)
+    }
 
 }
 
