150fe4f0d97bb150f1945114865bdd818cc593e7
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-25 16:24:38 +0300

    Model question and choice objects

---
+++ Brain Teaser/Choice.swift
@@ -0,0 +1,31 @@
+//
+//  Choice.swift
+//  Brain Teaser
+//
+//  Created by Muhammad Moaz on 1/25/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import Foundation
+
+struct Choice {
+    private (set) var id: Int?
+    private (set) var choice: String?
+    private (set) var isCorrect: Bool?
+    
+    init(dictionary: [String: AnyObject]) {
+        if let id = dictionary["id"] as? Int {
+            self.id = id
+        }
+        
+        if let choice = dictionary["choice"] as? String {
+            self.choice = choice
+        }
+        
+        if let isCorrect = dictionary["isCorrect"] as? Bool {
+            self.isCorrect = isCorrect
+        }
+    }
+    
+    
+}
---
+++ Brain Teaser/Question.swift
@@ -0,0 +1,32 @@
+//
+//  Question.swift
+//  Brain Teaser
+//
+//  Created by Muhammad Moaz on 1/25/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    private (set) var id: Int?
+    private (set) var question: String?
+    private (set) var choices: [Choice?] = []
+    
+    init(dictionary: [String: AnyObject]) {
+        if let id = dictionary["id"] as? Int {
+            self.id = id
+        }
+        
+        if let question = dictionary["question"] as? String {
+            self.question = question
+        }
+        
+        if let choices = dictionary["choices"] as? [Any] {
+            for object in choices {
+                let choice = Choice(dictionary: object as! [String: AnyObject])
+                self.choices.append(choice)
+            }
+        }
+    }
+}
