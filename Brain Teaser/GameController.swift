0c99746d125749ea72225c4d15ac23f54ecc9f56
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-26 11:46:02 +0300

    Add function to load next question is sequence

--- Brain Teaser/GameController.swift
+++ Brain Teaser/GameController.swift
@@ -97,6 +97,18 @@ class GameController: UIViewController {
         self.choiceButtonThree?.isEnabled = value
         self.choiceButtonFour?.isEnabled = value
     }
+    
+    func loadNextRoundWithDelay(seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        
+        // Executes the nextRound method at the dispatch time on the main queue
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            self.currentQuestionNumber += 1
+        }
+    }
 
     @IBAction func choiceButtonTapped(sender: UIButton) {
         guard let title = sender.titleLabel?.text else { return }
@@ -109,6 +121,7 @@ class GameController: UIViewController {
             }
         }
         
+        loadNextRoundWithDelay(seconds: 3)
     }
 
 }
